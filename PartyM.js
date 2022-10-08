6cb54900d5efe744b9bb821d2fab3de51ed4e4f4
AwesomeFlow <Anthonymlndz5@Gmail.com>
2022-10-08 02:34:59 -0400

    Start

---
+++ PartyM.js
@@ -0,0 +1,18 @@
+function PartyM(){
+this.health = 0;
+this.mana = 0;
+this.x = 0;
+this.y = 0;
+
+this.SwitchSpot = function(xValue, yValue){
+
+}
+
+this.Ability = function(){
+
+}
+
+this.show = function(){
+    
+}
+}
