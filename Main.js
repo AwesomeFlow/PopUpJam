9c0c82a46347507b649a3cc46bd5b10b248e9904
AwesomeFlow <Anthonymlndz5@Gmail.com>
2022-10-08 14:43:38 -0400

    Relearning

--- Main.js
+++ Main.js
@@ -1,7 +1,18 @@
+var firstSpot = [700,600];
+var secondSpot = [800,600];
+var thirdSpot = [900,600];
+var backSpot = [1000,700];
+
 function setup() {
     createCanvas(1280, 720);
+    partym1 = new PartyM(firstSpot[0],firstSpot[1]);
    }
 
 function draw(){
-    
+    //partym1.Ability();
 }
+
+/*function SummonParty(PartyM){
+    for(var i = 0; i < 4; i++){
+    }
+}*/
--- PartyM.js
+++ PartyM.js
@@ -1,18 +1,23 @@
-function PartyM(){
+function PartyM(x,y){
 this.health = 0;
 this.mana = 0;
-this.x = 0;
-this.y = 0;
+this.x = x;
+this.y = y;
+this.r = 30;
+this.SpriteColor = color(random(25,255), random(25, 255) , random(25, 255), 128);
 
 this.SwitchSpot = function(xValue, yValue){
 
-}
+    }
 
 this.Ability = function(){
-
-}
+    console.log("it's something")
+    }
 
 this.show = function(){
+    noStroke();
+    fill(this.SpriteColor);
+    ellipse(this.x,this.y, this.r * 2, this.r * 2);
     
-}
+    }
 }
